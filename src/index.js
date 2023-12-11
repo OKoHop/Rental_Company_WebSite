9ba0119793a56270394a22cb0b430f53c31b884c
OKoHop <87591503+OKoHop@users.noreply.github.com>
2023-12-11 17:52:23 +0200

    com

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
 import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
@@ -10,12 +10,12 @@ import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter basename="/Rental_Company_Website">
+  <HashRouter basename="/Rental_Company_Website">
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
   // </React.StrictMode>
 );
